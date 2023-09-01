60b869066a302a17e3a66149b75e58a990b6d31a
Hayden Menge <32338288+hemenge133@users.noreply.github.com>
2023-09-01 00:24:26 -0700

    Workaround toggle-only sprint

--- keyboards/cannonkeys/bakeneko65_iso_hs/keymaps/via/keymap.c
+++ keyboards/cannonkeys/bakeneko65_iso_hs/keymaps/via/keymap.c
@@ -28,13 +28,32 @@ enum layer_names {
 	_FN3
 };
 
+enum custom_keycodes {
+  MY_RELEASE = SAFE_RANGE,
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case MY_RELEASE:
+      if (record->event.pressed) {
+        // Key pressed: Do nothing
+      } else {
+        // Key released: Press again here
+        tap_code(MY_RELEASE);
+      }
+      return false; // Skip further processing
+    default:
+      return true;
+  }
+}
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[_BASE] = LAYOUT_65_iso_blocker(
 		QK_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_INS,
 		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_DEL,
 		KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_BSLS, KC_ENT, KC_PGUP,
-		KC_LSFT, KC_BSLS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_PGDN,
+		MY_RELEASE, KC_BSLS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_PGDN,
 		KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, MO(_FN1), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
